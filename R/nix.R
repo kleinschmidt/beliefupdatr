24bfca3d766f98167ab78c2fe3ce68cf1f07e425
Dave Kleinschmidt <dkleinschmidt@bcs.rochester.edu>
2016-03-18 17:44:43 -0400

    fix assert_that bug and add nix predictive pdf

--- R/nix.R
+++ R/nix.R
@@ -1,4 +1,4 @@
-
+require(assertthat)
 ## Functions for normal-inverse-chi-squared distribution and updating
 
 #' Conjugate updating of Normal-Chi^-2 parameters
@@ -23,7 +23,7 @@ nix2_update <- function(x=NA, p=list(nu=0, kappa=0, mu=0, sigma2=0),
                         ss=s2*(n-1),
                         s2=var(x)) {
   assert_that(is_nix2_params(p))
-  assert_that()
+  ## assert_that(! is.na(x) && is.na(ss) && is.na(s2))
   assert_that(n>0 || p$nu>0 && p$kappa>0)
   within(list(), {
     nu <- p$nu + n
@@ -31,6 +31,20 @@ nix2_update <- function(x=NA, p=list(nu=0, kappa=0, mu=0, sigma2=0),
     mu <- (p$mu*p$kappa + xbar*n) / (kappa)
     sigma2 <- (p$nu*p$sigma2 + ss + n*p$kappa/(n+p$kappa) * (p$mu-xbar)^2) / nu
   })  
+}
+
+#' Wrap parameters into a list for updating etc.
+#'
+#' @param mu
+#' @param sigma2
+#' @param kappa
+#' @param nu
+#'
+#' @export
+nix2_params <- function(...) {
+  p <- list(...)
+  assert_that(is_nix2_params(p))
+  return(p)
 }
 
 #' Check parameter vector for Normal-Chi^2
@@ -56,6 +70,24 @@ is_nix2_params <- function(p) {
   TRUE
 }
   
-assertthat::onfailure(is_nix2_params) <- function(call, env) {
+assertthat::on_failure(is_nix2_params) <- function(call, env) {
   paste0(deparse(call$x), ' are not valid Normal-Chi^-2 parameters')
 }
+
+#' Predictive distribution for Normal-Chi^-2 distribution
+#'
+#' This is the probability distribution of observations drawn from a normal
+#' distribution whose mean and variance follow the Normal-Chi^-2 distribution
+#' with the specified parameters. It works out to be a Student's t distribution.
+#'
+#' @param x Point to evaluate likelihood at. Can be a vector.
+#' @param p Parameters of Normal-Chi^-2 distribution.
+#' @param log Whether to return log likelihood (default: FALSE)
+#'
+#' @export
+d_nix2_predict <- function(x, p, log=FALSE) {
+  assert_that(is_nix2_params(p))
+  scale <- with(p, sqrt((1+kappa) * sigma2 / kappa))
+  dt((x-p$mu)/scale, df = p$nu, log=log)
+}
+
