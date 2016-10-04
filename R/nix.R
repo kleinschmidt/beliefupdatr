463496516a66c2c9f4fec802551599c1486a5f25
Dave Kleinschmidt <dkleinschmidt@bcs.rochester.edu>
2016-10-04 12:21:29 -0400

    classification functions from NIX2 distributions

--- R/nix.R
+++ R/nix.R
@@ -96,3 +96,40 @@ d_nix2_predict <- function(x, p, log=FALSE) {
   }
 }
 
+#' Classification function for Normal-Chi^-2 distributions
+#'
+#' This computes the posterior probability of data under different Normal-Chi^-2
+#' distributions.
+#'
+#' @param x Vector of points to classify
+#' @param ps List of category Norma-Chi^-2 params
+#' @param cat (Optional) Which categories' posteriors to return. If missing, all
+#' are returned
+#'
+#' @return A length(x)-by-category matrix of posterior probabilities
+#'
+#' @export
+nix2_classify <- function(x, ps, category=1:length(ps)) {
+  map(ps, d_nix2_predict, x=x, log=FALSE) %>%
+    do.call(cbind, .) %>%
+    apply(1, function(x) x / sum(x)) %>%
+    t() %>%
+    `[`(, category)
+}
+
+## MUCH MUCH SLOWER:
+nix2_classify_2 <- function(x, ps, category=1:length(ps)) {
+  x %>%
+    map(function(onex) map_dbl(ps, d_nix2_predict, x=onex, log=FALSE)) %>%
+    map(~ .x / sum(.x)) %>%
+    do.call(rbind, .)
+}
+
+## a little slower, but numerically more stable for very small lhoods.
+nix2_classify_3 <- function(x, ps, category=1:length(ps)) {
+  map(ps, d_nix2_predict, x=x, log=TRUE) %>%
+    do.call(cbind, .) %>%
+    apply(1, function(x) exp(x - daver::log_sum_exp(x))) %>%
+    t() %>%
+    `[`(, category)
+}
