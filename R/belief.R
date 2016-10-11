47a1c9ab3627b8e8467f5fb98ddc360bcc0f7694
Dave Kleinschmidt <dkleinschmidt@bcs.rochester.edu>
2016-10-11 11:39:53 -0400

    implements batch belief updating

--- R/belief.R
+++ R/belief.R
@@ -1,3 +1,5 @@
+#' @import assertthat
+NULL
 
 # take a data frame with observations, and accumulate through nix2_update_one.
 
@@ -75,3 +77,63 @@ d3 <- belief_update_2(d, 'x', 'c', pl0)
 # surprisingly, version with transpose isn't much slower (~1ms out of 20ms)
 microbenchmark(belief_update_2(d, 'x', 'c', pl0),
                belief_update(d, 'x', 'c', p0))
+# If I had to guess, it's because most of the time is spent doing the
+# accumulation, and constructing a list is cheap enough that it doesn't add too
+# much overhead relative to the main inefficiency.
+
+# next want to update in batch. tell how to split up trials and upate params for
+# each cut.
+#
+# split by cut, then split up category, summarise
+
+
+summary_stats <- function(x) within(list(), {
+  n <- length(x)
+  xbar <- mean(x)
+  ss <- var(x) * (n-1)
+})
+
+# accumulator for batch updating. params and sumstats are lists of parameters to
+# update and corresponding category summary statistics respectively
+update_summary_stats <- function(params, sumstats) {
+  map2(params, sumstats[names(params)],
+       ~ do.call(nix2_update, c(list(p=.x), .y)))
+}
+
+#' Update beliefs in a data frame in batches
+#'
+#' @param d Data frame with cue values and (optionally) categories
+#' @param cue Quoted name of column of data frame with cue values
+#' @param categories Quoted name of column with category labels.
+#' @param trials Quoted name of column with trial numbers to define batches
+#' @param at Trial numbers to get updated beliefs
+#' @param params Named list of starting belief NIX^2 parameters. Names must
+#'   match unique values of \code{d[[categories]]}
+#'
+#' @return A data_frame with updated NIX2 parameters in \code{params} for the
+#'   values of \code{d[[trials]]} from \code{at}. \code{d$params} is a list
+#'   column where each element is a named list of category belief parameters
+#'
+#' @export
+belief_update_batch <- function(d, cue, categories, trials, at, params) {
+  assert_that(d %has_name% cue, d %has_name% categories, d %has_name% trials)
+  assert_that(max(d[[trials]]) >= max(at))
+  assert_that(is_empty(setdiff(d[[categories]], names(params))))
+
+  # splice in name of trials column in cut() expression
+  grouping <- lazyeval::interp(~ cut(trs,
+                                     breaks=c(-Inf, at),
+                                     labels=at),
+                               trs=as.name(trials)) %>%
+    list() %>%
+    set_names(trials)
+
+  # cut, summarise by category, and update
+  d %>%
+    group_by_(.dots=grouping) %>%
+    nest() %>%
+    mutate(params = map(data, ~ split(.[[cue]], .[[categories]]) %>%
+                                map(summary_stats)) %>%
+             accumulate(update_summary_stats, .init=params) %>%
+             tail(length(data))) %>%
+}
