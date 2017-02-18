4707412ba6c8201d7da67d900931b655582bae1c
Dave Kleinschmidt <dave.f.kleinschmidt@gmail.com>
2017-02-18 11:38:23 -0500

    move to using compiled stanmodels in vignette and stans.R

--- R/stans.R
+++ R/stans.R
@@ -3,36 +3,11 @@
 #' @import methods
 NULL
 
-#' Get a model's full path
-#'
-#' Code for stan models is stored in the exec/ subdirectory of wherever
-#' this package is installed. This is a convenience shortcut to get fully
-#' specified paths to a model
-#'
-#' @param model_name (Relative) filename of model
-#' 
-#' @export
-model_filename <- function(model_name) {
-  file.path(devtools::inst('beliefupdatr'),
-            'exec',
-            model_name)
-}
-
-#' Compile an included Stan model
-#'
-#' @param model_name (Relative) filename of model
-#' @param ... Additional arguments are passed to \code{rstan::stan}
-#' 
-#' @export
-compile_stan <- function(model_name) {
-  rstan::stan_model(model_filename(model_name))
-}
-
 #' List the included models
 #'
 #' These can be passed to model_filename or compile_stan
 #'
 #' @export
 list_models <- function() {
-  basename(Sys.glob(model_filename('*.stan')))
+  names(stanmodels)
 }
