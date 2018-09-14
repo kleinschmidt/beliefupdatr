9c47b320ecbd7b2e6490187ef445afb56ee2e42d
Dave Kleinschmidt <dave.f.kleinschmidt@gmail.com>
2018-09-14 17:35:42 -0400

    update to rstantools 1.5

---
+++ R/beliefupdatr-package.R
@@ -0,0 +1,18 @@
+#' The 'beliefupdatr2' package.
+#' 
+#' @description Modeling phonetic adaptation as belief updating requires some
+#'   distributions and utility functions. Here they are.
+#' 
+#' @docType package
+#' @name beliefupdatr-package
+#' @aliases beliefupdatr
+#' @useDynLib beliefupdatr, .registration = TRUE
+#' @import methods
+#' @import Rcpp
+#' @import rstantools
+#' @importFrom rstan sampling
+#' 
+#' @references 
+#' Stan Development Team (2018). RStan: the R interface to Stan. R package version 2.17.3. http://mc-stan.org
+#' 
+NULL
